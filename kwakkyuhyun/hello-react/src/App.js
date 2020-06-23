61be3eb29194a0d7e836a5f49e5018379ad08242
kyuhyeon.kwak <kyuhyeon.kwak@nts-corp.com>
2020-06-23 21:50:07 +0900

    [CHAPTER 02] JSX문법

--- kwakkyuhyun/hello-react/src/App.js
+++ kwakkyuhyun/hello-react/src/App.js
@@ -3,24 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = '리액트';
+    return (
+        // <div className="App">
+        //   <header className="App-header">
+        //     <img src={logo} className="App-logo" alt="logo" />
+        //     <p>
+        //       Edit <code>src/App.js</code> and save to reload.
+        //     </p>
+        //     <a
+        //       className="App-link"
+        //       href="https://reactjs.org"
+        //       target="_blank"
+        //       rel="noopener noreferrer"
+        //     >
+        //       Learn React
+        //     </a>
+        //   </header>
+        // </div>
+        
+        <>
+            {name === '리액트' && <h1 className='react'>{name}입니다.</h1>}
+            <input />
+        </>
+    );
 }
 
 export default App;
