c451a2bf5e9b68cbda74610636924cf7e8339665
roughjs247 <roughjs247@naver.com>
2020-06-30 21:56:33 +0900

    chapter.7

--- leeyongbin/hello-react/src/App.js
+++ leeyongbin/hello-react/src/App.js
@@ -1,12 +1,30 @@
 import React, {Component} from 'react';
 // import ValidationSample from './chapter.5/ValidationSample';
 // import ScrollBox from './chapter.5/class/ScrollBox';
-import IterationSample from './chapter.6/func/IterationSample';
+//import IterationSample from './chapter.6/func/IterationSample';
+import LifeCycleSample from './chapter.7/LifeCycleSample';
+
+function getRandomColor(){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
-    render() {
+    state = {
+      color : '#000000'
+    }
+
+    handleClick = () => {
+      this.setState({
+        color:getRandomColor()
+      });
+    }
+
+    render(){
       return (
-        <IterationSample></IterationSample>
+        <div>
+          <button onClick={this.handleClick}>랜덤색상</button>
+          <LifeCycleSample color={this.state.color}/>
+        </div>
       );
     }
 }
