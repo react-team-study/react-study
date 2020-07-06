880ed5fa79ad9ba17d89d71decbf958e39025f4b
Hongsam <dev.hongsam@gmail.com>
2020-07-07 01:43:10 +0900

    practice chapter5 example

--- kimjongsoo/react-tutorial/src/App.js
+++ kimjongsoo/react-tutorial/src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import Sample from "./chapter3/Sample";
-import Counter from "./chapter3/Counter";
-import EventSample from "./chapter4/EventSample";
-import EventSampleFunc from "./chapter4/EventSampleFunc";
-import EventSampleFunc2 from "./chapter4/EventSampleFunc2";
+import React, { Component } from "react";
+// import Sample from "./chapter3/Sample";
+// import Counter from "./chapter3/Counter";
+// import EventSample from "./chapter4/EventSample";
+// import EventSampleFunc from "./chapter4/EventSampleFunc";
+// import EventSampleFunc2 from "./chapter4/EventSampleFunc2";
+import ValidationSample from "./chapter5/ValidationSample";
+import ScrollBox from "./chapter5/ScrollBox";
 
-function App() {
-  return <EventSampleFunc2 />;
+// function App() {
+//   return <ValidationSample />;
+// }
+
+class App extends Component {
+  render() {
+    // return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox
+          ref={(ref) => {
+            this.scrollbox = ref;
+          }}
+        />
+        <button onClick={() => this.scrollbox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
