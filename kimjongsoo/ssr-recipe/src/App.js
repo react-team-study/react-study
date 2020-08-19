efd0823c36022215ca107f92aa246bd6709218b4
Hongsam <dev.hongsam@gmail.com>
2020-08-19 23:44:46 +0900

    Commit before eject

---
+++ kimjongsoo/ssr-recipe/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import logo from "./logo.svg";
+import "./App.css";
+import RedPage from "./pages/RedPage";
+import BluePage from "./pages/BluePage";
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+    </div>
+  );
+}
+
+export default App;
