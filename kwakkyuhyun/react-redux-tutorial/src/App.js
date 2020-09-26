e76969b9e37153e5c17d43e85afa32f8e95fc7f7
kyuhyeon.kwak <kyuhyeon.kwak@nts-corp.com>
2020-09-26 14:32:26 +0900

    [CHAPTER 17] 초기 세팅

---
+++ kwakkyuhyun/react-redux-tutorial/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <>
+      <Counter number={0} />
+    </>
+  );
+}
+
+export default App;
